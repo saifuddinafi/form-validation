d700d4a86c5293885a50944c195f057e081a4af0
Saifuddinafi <afi.saifudin7@gmail.com>
2024-01-25 14:31:09 +0700

    form-validation

---
+++ src/FormComponent/RegistrationForm.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import './RegistrationForm.css';
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.username && formData.email && formData.password) {
+      console.log('Data Registrasi:', formData);
+    } else {
+      alert('Harap isi semua field!');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Form Registrasi</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username : 
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email : 
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password : 
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
